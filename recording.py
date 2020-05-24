3b46da8fd1c8f650b8d9a50f53737eb60a5e8987
marcel41 <mcube41xd@gmail.com>
2020-05-24 18:33:41 +0100

    test10

--- recording.py
+++ recording.py
@@ -6,14 +6,15 @@ samplesPerSeconds = 0
 #def section
 #-------------------------------------------------------------------------------
 def process_emg(emg):
-  global samplesPerSeconds
+  #global samplesPerSeconds
   if(isReadyToRegisterData):
     print("readings -> ", emg)
+    global samplesPerSeconds
     samplesPerSeconds += 1
 #-------------------------------------------------------------------------------
 
 if __name__ == "__main__":
-  global samplesPerSeconds
+  #global samplesPerSeconds
   myo_mac_addr = myo.get_myo()
   myo_device = myo.Device()
   myo_device.services.sleep_mode(1)  # never sleep
