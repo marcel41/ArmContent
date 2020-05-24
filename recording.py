e28433e7525294f87f2abb86aad39143f0b14392
marcel41 <mcube41xd@gmail.com>
2020-05-24 17:42:20 +0100

    test1

--- recording.py
+++ recording.py
@@ -9,4 +9,13 @@ def process_emg(emg):
 if __name__ == "__main__":
   myo_mac_addr = myo.get_myo()
   myo_device = myo.Device()
-  myo_device.services.sleep_mode(0)
+  myo_device.services.sleep_mode(1)  # never sleep
+  myo_device.services.vibrate(1) #short vibration
+  myo_device.services.eme_filt_notifications()
+  print("Battery: %d" % myo_device.services.battery)
+  myo_device.services.set_mode(myo.EmgMode.FILT, myo.ImuMode.OFF, myo.ClassifierMode.OFF)
+  myo_device.add_emg_event_handler(process_emg)
+  while True:
+    if myo_device.services.waitForNotifications(1):
+      continue #return to the beggining of while loop
+    print("no data has been received from the peripheral, waiting...")
