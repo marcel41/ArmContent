9a458d0c10218f747f9c7ca5719209234a83c2d4
marcel <marcel_moran41@hotmail.com>
2020-05-26 03:54:47 +0100

    getting data

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import numpy as np
 import glob
 import io
 import os
+import math
 """
 ftest = open("dataSet/fist.txt","r")
 myList = list()
@@ -43,7 +44,12 @@ if __name__ == "__main__":
   working_dir = "dataSet"
   firstSample = True
   data_x = None
+  data_x_train = None
+  data_x_test = None
+  ratio = 0.8
   data_y = None
+  data_y_train = None
+  data_y_test = None
   gestureOrder = list()
   gestureNum = 0
   myList = list()
@@ -68,27 +74,49 @@ if __name__ == "__main__":
       y_label_Class = os.path.basename(fd.name)
       if(firstSample):
         data_x = np.copy(np_examples)
+        data_x_train = np.copy(np_examples[0:int(len(np_examples)*ratio)])
+        data_x_test = np.copy(np_examples[int(len(np_examples)*ratio):len(np_examples)])
         firstSample = False
       else:
         data_x = np.concatenate((data_x,np_examples),axis=0)
+        data_x_train = np.concatenate((data_x_train,np_examples[0:int(len(np_examples)*ratio)]),axis=0)
+        data_x_test = np.concatenate((data_x_test,np_examples[int(len(np_examples)*ratio):len(np_examples)]),axis=0)
       gestureOrder.append(y_label_Class)
       #value_label = [len(gestureOrder)] * 10
       #data_y.append(value_label)
       myList.clear()
       print(data_x.shape)
       print(data_x[9][1][:])
+  #data_x = tf.keras.utils.normalize(data_x)
   print(data_x[9][1][:])
   print(data_x[19][1][:])
   print(data_x[29][1][:])
   print(data_x[39][1][:])
   samplesPerGesture = int(len(data_x)/len(gestureOrder))
   data_y = list()
+  data_y_train = list()
+  data_y_test = list()
+  #data_x_train = np.empty((5,len(data_x[0]),8))
+  #data_x_train = np.empty((0,)*3)
+  #print(data_x_train)
+  #print(data_x_train.shape)
   for label in range(len(gestureOrder)):
     data_y = data_y + ([label] * samplesPerGesture)
+    data_y_train = data_y_train + [label] * int(samplesPerGesture * ratio)
+    data_y_test = data_y_test + [label] * int(samplesPerGesture * (1 - ratio) + 1)
+    #data_x_train = np.concatenate((data_x_train,data_x[int(label*samplesPerGesture):int(((label*samplesPerGesture) + samplesPerGesture/2 - 1))]),axis=0)
     print("Number: ", label, "is assigned to", gestureOrder[label])
   data_y = np.asarray(data_y)
-  print(data_y.shape)
-  print(data_y[9])
-  print(data_y[19])
-  print(data_y[29])
-  print(data_y[39])
+  data_y_train = np.asarray(data_y_train)
+  data_y_test = np.asarray(data_y_test)
+  print(data_x_test.shape)
+  print(data_x_train.shape)
+  print(data_x_train[7][0][:])
+  print(data_y_train.shape)
+  print(data_y_test.shape)
+  print(data_x_test[0][0][:])
+  #data_x_train = np.delete(data_x_train,[0],axis=0)
+  #print(data_x_train.shape)
+  #spittling data_x into different
+  #data_x_train = data_x[0:int(samplesPerGesture/2)]
+  #from here start create the model
